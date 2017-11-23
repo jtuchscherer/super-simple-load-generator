b13ea29c0a6991fec79b704ed835d1e421f56610
Johannes Tuchscherer <jtuchscherer@gmail.com>
2017-11-23 23:43:55 +0100

    Making the http calls non blocking

--- load_generator.rb
+++ load_generator.rb
@@ -14,18 +14,25 @@ username = ENV["username"]
 password = ENV["password"]
 
 def load_generator (base_url, time_start, time_end, paths, username, password)
+    outstanding_requests = 0
+
     while true do
       endpoint = if paths.nil? then base_url else base_url + paths.sample() end
       options = {:ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE}
-
-      unless username.empty? || password.empty?
+      
+      unless username.nil? || password.nil?
         options[:http_basic_authentication] = [username, password]
       end
 
-      open(endpoint, options) do |response|
-        puts "#{response.base_uri} returned #{response.status}"
+      Thread.new do
+        outstanding_requests = outstanding_requests + 1
+        open(endpoint, options) do |response|
+          puts "#{response.base_uri} returned #{response.status}"
+        end
+        outstanding_requests = outstanding_requests - 1 
+        puts "#{outstanding_requests} outstanding requests"
       end
-    sleep rand(time_start..time_end)
+      sleep rand(time_start..time_end)
     end
 end
 
